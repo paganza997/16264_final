2316d83e19b903330477a3f405215258f59670f4
paganza997 <71294437+paganza997@users.noreply.github.com>
2022-05-14 14:23:20 -0700

    Add files via upload

---
+++ get_song_info.py
@@ -0,0 +1,41 @@
+import spotipy
+from spotipy import SpotifyClientCredentials
+
+client_id='32d27aa5402145f0a1a6e1de91b976d1'
+client_secret='5670ef7041c846d8a79aecdc791951cc'
+
+manager = SpotifyClientCredentials(client_id,client_secret)
+sp = spotipy.Spotify(client_credentials_manager=manager)
+
+def get_songs_features(ids):
+
+    meta = sp.track(ids)
+    features = sp.audio_features(ids)
+
+    # meta
+    name = meta['name']
+    album = meta['album']['name']
+    artist = meta['album']['artists'][0]['name']
+    release_date = meta['album']['release_date']
+    length = meta['duration_ms']
+    popularity = meta['popularity']
+    ids =  meta['id']
+
+    # features
+    acousticness = features[0]['acousticness']
+    danceability = features[0]['danceability']
+    energy = features[0]['energy']
+    instrumentalness = features[0]['instrumentalness']
+    liveness = features[0]['liveness']
+    valence = features[0]['valence']
+    loudness = features[0]['loudness']
+    speechiness = features[0]['speechiness']
+    tempo = features[0]['tempo']
+    key = features[0]['key']
+    time_signature = features[0]['time_signature']
+
+    track = [name, album, artist, ids, release_date, popularity, length, danceability, acousticness,
+            energy, instrumentalness, liveness, valence, loudness, speechiness, tempo, key, time_signature]
+    columns = ['name','album','artist','id','release_date','popularity','length','danceability','acousticness','energy','instrumentalness',
+                'liveness','valence','loudness','speechiness','tempo','key','time_signature']
+    return track,columns
